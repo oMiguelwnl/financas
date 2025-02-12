e5d41d0db0894088ac2a28caf43da47123be4a81
oMiguelwnl <contato.miguel.almeida7@gmail.com>
2025-02-12 11:16:04 -0300

    feat: add date-fns for date formatting and implement balance retrieval in Home screen

--- frontend/pages/Home/Home.js
+++ frontend/pages/Home/Home.js
@@ -1,12 +1,39 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { View, Text, Button } from "react-native";
 
 import { AuthContext } from "../../contexts/auth";
 
 import Header from "../../components/Header/Header";
 import { Background } from "./styles";
+import { format } from "date-fns";
+import api from "../../services/api";
 
 export default function Home() {
+  const [listBalance, setListBalance] = useState([]);
+  const [dateMoviments, setDateMoviments] = useState(new Date());
+
+  useEffect(() => {
+    isActive = true;
+
+    async function getMoviments() {
+      let formatedDate = format(dateMoviments, "dd/MM/yyyy");
+
+      const balance = await api.get("/balance", {
+        params: {
+          date: formatedDate,
+        },
+      });
+
+      if (isActive) {
+        setListBalance(balance.data);
+      }
+    }
+
+    getMoviments();
+
+    return () => (isActive = false);
+  }, []);
+
   return (
     <Background>
       <Header title="Minhas movimentações" />
