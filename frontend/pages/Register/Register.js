015b21453a5ed0c53a3e1835784dd8eb4924ae5e
oMiguelwnl <contato.miguel.almeida7@gmail.com>
2025-02-13 10:46:36 -0300

    feat: add RegisterTypes component for selecting income and expense types in Register page

--- frontend/pages/Register/Register.js
+++ frontend/pages/Register/Register.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Background, Input, SubmitButton, SubmitText } from "./styles";
 import { SafeAreaView, TouchableWithoutFeedback, Keyboard } from "react-native";
+
 import Header from "../../components/Header/Header";
+import RegisterTypes from "../../components/RegisterTypes/RegisterTypes";
 
 export default function Register() {
+  const [labelInput, setLabelInput] = useState("");
+  const [valueInput, setValueInput] = useState("");
+  const [type, setType] = useState("receita");
+
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <Background>
         <Header title="Registrando" />
 
         <SafeAreaView style={{ marginTop: 14, alignItems: "center" }}>
-          <Input placeholder="Descrição desse registro" />
+          <Input
+            placeholder="Descrição desse registro"
+            value={labelInput}
+            onChangeText={(text) => setLabelInput(text)}
+          />
 
-          <Input placeholder="Valor desejado" keyboardType="numeric" />
+          <Input
+            placeholder="Valor desejado"
+            keyboardType="numeric"
+            value={valueInput}
+            onChangeText={(text) => setValueInput(text)}
+          />
+
+          <RegisterTypes
+            type={type}
+            sendTypeChanged={(item) => setType(item)}
+          />
 
           <SubmitButton>
             <SubmitText>Registrar</SubmitText>
